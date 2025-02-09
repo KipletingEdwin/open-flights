cb562e7df7aaf888c460233e7a570dd1327e5777
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-02-09 15:30:38 +0000

    implement def method in airlines controller

--- app/controllers/api/v1/airlines_controller.rb
+++ app/controllers/api/v1/airlines_controller.rb
@@ -5,6 +5,8 @@ module Api
         class AirlinesController < ApplicationController 
 
             def index
+                airlines = Airline.all 
+                render json: AirlineSerializer.new(airlines).serialized_json
             end
 
             def show
