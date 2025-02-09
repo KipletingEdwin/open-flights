8cef55cb2e3fe84ebf937f9b2614b88277329ff1
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-02-09 17:20:03 +0000

    add create  method to reviews controller

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -3,6 +3,16 @@ module Api
     module V1
         class ReviewsController < ApplicationController 
 
+            def create 
+                review = Review.new(review_params) 
+
+                if review.save 
+                    render json: ReviewSerializer.new(review).serialized_json 
+                    else 
+                        render json: {error: review.errors.messages }, status: 422 
+                    end
+            end
+
 
             private 
 
